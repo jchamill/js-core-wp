f1d5ccd3c679b1ec5f375af9745981f4feb95df9
JC Hamill <jchamill@creativeshampoo.com>
2019-02-06 16:06:33 -0500

    remove faq widget template variable function

--- modules/SiteOrigin/widgets/faqs/faqs.php
+++ modules/SiteOrigin/widgets/faqs/faqs.php
@@ -58,44 +58,6 @@ class JS_Core_Faqs_Widget extends SiteOrigin_Widget {
     );
   }
 
-  public function get_template_variables( $instance, $args ) {
-    if( empty( $instance ) ) return array();
-
-    $categories = get_categories(array(
-      'taxonomy' => 'faq_category',
-      'hide_empty' => true,
-    ));
-
-    $faqs = array();
-    foreach ($categories as $category) {
-      $loop = new WP_Query(array(
-        'post_type' => 'faq',
-        'posts_per_page' => -1,
-        'tax_query' => array(array(
-          'taxonomy' => 'faq_category',
-          'field' => 'term_id',
-          'terms' => $category->term_id,
-        )),
-      ));
-
-      $weight = \JS_Core\Modules\Structure::get_term_field( $category->term_id, 'crb_weight' );
-
-      $faqs[$category->term_id] = array(
-        'term_id' => $category->term_id,
-        'weight' => $weight,
-        'name' => $category->name,
-        'loop' => $loop,
-      );
-    }
-
-    uasort($faqs, function($a, $b) {
-      return $a['weight'] - $b['weight'];
-    });
-
-    return array(
-      'faqs' => $faqs,
-    );
-  }
 }
 
 siteorigin_widget_register( 'js-core-faqs', __FILE__, 'JS_Core_Faqs_Widget' );
