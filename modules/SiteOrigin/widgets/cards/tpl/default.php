1ba9bffe5a8d6d7ac5b9b94650500a9a9177cb1c
JC Hamill <jchamill@creativeshampoo.com>
2019-02-06 12:38:08 -0500

    adjust markup for card links

--- modules/SiteOrigin/widgets/cards/tpl/default.php
+++ modules/SiteOrigin/widgets/cards/tpl/default.php
@@ -3,33 +3,33 @@
   <div class="cards-widget grid-container">
     <?php foreach( $instance['items'] as $i => $item ): ?>
       <div class="grid-item-1">
+        <?php if ( ! empty( $item['url'] ) ): ?>
+          <a href="<?php print esc_url( $item['url'] ); ?>">
+        <?php endif; ?>
+
         <div class="card-container">
-          <?php if ( ! empty( $item['url'] ) ): ?>
-            <a href="<?php print esc_url( $item['url'] ); ?>">
+          <?php
+          if ( ! empty( $item['image'] ) ) {
+            print wp_get_attachment_image( $item['image'], $instance['attachment_size'], false, array(
+              'title' => $item['title']
+            ) );
+          }
+          ?>
+
+          <?php if ( ! empty( $item['title'] ) ): ?>
+            <h3><?php print $item['title']; ?></h3>
           <?php endif; ?>
 
-            <?php
-            if ( ! empty( $item['image'] ) ) {
-              print wp_get_attachment_image( $item['image'], $instance['attachment_size'], false, array(
-                'title' => $item['title']
-              ) );
-            }
-            ?>
-
-            <?php if ( ! empty( $item['title'] ) ): ?>
-              <h3><?php print $item['title']; ?></h3>
-            <?php endif; ?>
-
-            <?php if ( ! empty( $item['content'] ) ): ?>
-              <div class="entry-content">
-                <?php print $item['content']; ?>
-              </div>
-            <?php endif; ?>
-
-          <?php if ( ! empty( $item['url'] ) ): ?>
-            </a>
+          <?php if ( ! empty( $item['content'] ) ): ?>
+            <div class="entry-content">
+              <?php print $item['content']; ?>
+            </div>
           <?php endif; ?>
         </div>
+
+        <?php if ( ! empty( $item['url'] ) ): ?>
+          </a>
+        <?php endif; ?>
       </div>
     <?php endforeach; ?>
   </div>
